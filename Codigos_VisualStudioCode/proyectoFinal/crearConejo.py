0cf8044dd5f24a9235e8bf1587b998fee6ca3632
MayeCamargo <est.diana.camargo3@unimilitar.edu.co>
2026-05-14 13:14:37 -0500

    Subir carpeta de Jennifer a Repositorio

---
+++ Codigos_VisualStudioCode/proyectoFinal/crearConejo.py
@@ -0,0 +1,123 @@
+import maya.cmds as cmds
+import random # Generar números aleatorios.
+
+#                         ╔═══════════════════════════════════════════════════════════════╗
+#                         ║   Definir Modulo m. (ancho de Cabeza alearorio)               ║             
+#                         ╚═══════════════════════════════════════════════════════════════╝
+
+# Ancho de cabeza: numero entero  aleatorio entre 20 y 43 cm
+# modulo m = ancho de cabeza / 10
+ancho_cabeza = random.randint(20, 43) 
+m = ancho_cabeza / 10
+
+
+#  region Funcion crear_cubo
+#                         ╔═════════════════════════════════════════════════════════════════╗
+#                         ║  Entender como Maya saca las formas, Funcion crear_cubo         ║                                                               ║
+#                         ╚═════════════════════════════════════════════════════════════════╝
+
+def crear_cubo(nombre, escala, posicion): 
+    ancho, alto, profundidad = escala # Definimos nombre de los valores de la escala, en lugar de w.h.d  por defecto.
+
+    """
+    Creamos una función reutilizable para dibujar n cubos.
+    parametros: nombre STRING que llevara el cubo en el ouliner de maya (string), 
+                 escala VECTOR (3 valores para x= ancho, y= alto, z= profundidad),
+                posicion VECTOR (3 valores para coordenadas x,y,z)
+    """
+
+    subdivisiones=(2,2,2)
+    sx , sy , sz = subdivisiones
+  
+    cubo = cmds.polyCube( name=nombre, w=ancho, h=alto, d=profundidad, sx=3,sy=3,sz=3)[0]  
+
+    """
+    Creamos un cubo con el comando polyCube y le asignamos un nombre y una escala
+        la razon por la que usamos una lista, es porque polycube devuelve una lista, 
+        En la que el primer elemento es el objeto (la geometria)
+        y el segundo elemento es las transformaciones que se le han hecho
+        nosotros cogemos la primera que es el objeto y le asigamos nombre y escala. 
+    """
+    
+    """
+    Calculamos el offset de la posicion,  por defecto el cubo se crea con su centro en el origen (0,0,0),
+    y nosotros queremos que el punto de referencia sea la esquina superior izquierda de la cara frontal del cubo,
+              y que se dibuje de ahi para abajo y hacia la derecha y la sobre el plano xy osea z=0
+    Al finalizar el conejo debemos reubicar los pivotes para que los joints se creen en el centro de las formas.
+    """
+    offset_x = ancho / 2
+    offset_y = -(alto / 2)
+    offset_z = -(profundidad / 2)
+    cmds.move(posicion[0] + offset_x, posicion[1] + offset_y, posicion[2] + offset_z, cubo)
+    
+    return cubo
+# endregion
+
+# region Crear Conejo Estandar 
+#                         ╔═════════════════════════════════════════════════════════════════╗
+#                         ║  Crear Conejo Estandar                                          ║
+#                         ╚═════════════════════════════════════════════════════════════════╝
+
+def crear_conejo(emocion="calma"):
+    """
+    MiCubitoDePrueba = crear_cubo("MiCubitoDePrueba", (1, 2, 3), (0, 0, 0)) 
+    Crea un cubo en 0,0,0 con escala 1 de ancho,2 cm de alto ,3 cm de profundo y nombre "MiCubitoDePrueba"
+    Si queremos ver las medidas del cubo: chanel box → polyCube1 → input NO en escala, la escala siempre dira 1 
+    """
+
+    #                            nombre                            escala                            posicion 
+    cabeza = crear_cubo("Cabeza_Primitiva_001",          (m*10,   m*10,         (m*10)/2 ),    (0,     0,                  0          )  ) 
+    ojo_izq = crear_cubo("OjoIzquierdo_Primitiva_002",   (m,      m,            (m*1.7)  ),    (m*7,   (-(m*10)/3.7),      (m*1.7)-m  )  )
+    ojo_der = crear_cubo("OjoDerecho_Primitiva_003",     (m,      m,            (m*1.7)  ),    (m*2,   (-(m*10)/3.7),      (m*1.7)-m  )  )
+    nariz = crear_cubo("Nariz_Primitiva_004",            (m,      m,            (m*1.7)  ),    (m*4.5, (-(m*10)/2),        (m*1.7)-m  )  )
+    boca = crear_cubo("Boca_Primitiva_005",              (m*3,    m/2,          (m*1.7)  ),    (m*3.5, (-(m*10)/1.33),     (m*1.7)-m  )  )
+    oreja_izq = crear_cubo("Oreja_Izquierda_006",        (m*3,    ((m*10)/3)*2, m*2      ),    (m*6,   (((m*10)/3)*2)-m,   (-m*1.5)   )  )
+    oreja_der = crear_cubo("Oreja_Derecha_007",          (m*3,    ((m*10)/3)*2, m*2      ),    (m,     (((m*10)/3)*2)-m,   (-m*1.5)   )  )
+    
+
+    pie_izq = crear_cubo("PieIzquierdo_Primitiva_008",   (m*3,    m*3,          m*2      ),    (m*6,    (-m*21),              (-m*1.5)))
+    pie_der = crear_cubo("PieDerecho_Primitiva_009",     (m*3,    m*3,          m*2      ),    (m,      (-m*21),              (-m*1.5)))
+    tronco = crear_cubo("Tronco_Primitiva_010",          (m*13,   m*13,         (m*16)/2 ),    (-m*1.5, (-m*9),            (m*1.5)   )  )
+    mano_izq = crear_cubo("ManoIzquierda_Primitiva_011", (m*4,    m*3,          m*2      ),    (m*10.5, (-m*12),           (-m*1.5)   )  )
+    mano_der = crear_cubo("ManoDerecha_Primitiva_012",   (m*4,    m*3,          m*2      ),    (-m*4.5, (-m*12),           (-m*1.5)   )  )
+    cola = crear_cubo("Cola_Primitiva_013",              (m,      m,            m*2      ),    (m*4.5,  (-m*9)+(-m*7.2),   (-m*5.5)   )  ) 
+    
+    parte_superior = cmds.group(cabeza, ojo_izq, ojo_der, nariz, boca, oreja_izq, oreja_der, name="ParteSuperior_Grupo")
+    parte_inferior = cmds.group(tronco, mano_izq, mano_der, pie_izq, pie_der, cola, name="ParteInferior_Grupo")
+    conejo = cmds.group(parte_superior, parte_inferior, name="Conejo_Grupo_001")
+# endregion
+
+#region suavizado
+#                         ╔═════════════════════════════════════════════════════════════════╗
+#                         ║  Suavizar las formas                                            ║                                                               ║
+#                         ╚═════════════════════════════════════════════════════════════════╝
+
+def suavizar_conejo_preview():
+
+    objetos = [
+        "Cabeza_Primitiva_001",
+        "OjoIzquierdo_Primitiva_002",
+        "OjoDerecho_Primitiva_003",
+        "Nariz_Primitiva_004",
+        "Boca_Primitiva_005",
+        "Oreja_Izquierda_006",
+        "Oreja_Derecha_007",
+        "PieIzquierdo_Primitiva_008",
+        "PieDerecho_Primitiva_009",
+        "Tronco_Primitiva_010",
+        "ManoIzquierda_Primitiva_011",
+        "ManoDerecha_Primitiva_012",
+        "Cola_Primitiva_013"
+    ]
+
+    for obj in objetos:
+        if cmds.objExists(obj):
+
+            # equivalente a tecla 3
+            cmds.setAttr(obj + ".displaySmoothMesh", 2)
+            # calidad del preview
+            cmds.setAttr(obj + ".smoothLevel", 2)
+
+    print("Conejo suavizado")
+# endregion
+
---
+++ Codigos_VisualStudioCode/proyectoFinal/sistemaIKFK.py
@@ -0,0 +1,169 @@
+import maya.cmds as cmds
+
+
+def crear_sistema_ikfk(fk_chain,ik_chain,main_chain,prefix,joint_attr,pv_offset=5):
+
+# =========================
+    # CALCULAR TAMAÑO
+    # =========================
+
+    tamano = distancia_entre(main_chain[0], main_chain[1]) * 0.5
+
+    # =========================
+    # FK CONTROLS
+    # =========================
+
+    fk_controls = []
+
+    for i, jnt in enumerate(fk_chain[:-1]):
+
+        ctrl, offset = crear_control(
+            f"{prefix}_FK_CTRL_{i+1:03}",
+            jnt,
+            size=tamano
+        )
+
+        cmds.parentConstraint(ctrl, jnt, mo=True)
+
+        fk_controls.append(ctrl)
+    # jerarquía FK
+        for i in range(1, len(fk_controls)):
+            cmds.parent(
+                f"{fk_controls[i]}_OFFSET",
+                fk_controls[i-1]
+            )
+    # =========================
+    # IK HANDLE
+    # =========================
+    ik_handle, effector = cmds.ikHandle(sj=ik_chain[0],ee=ik_chain[-1],
+        solver="ikRPsolver"
+    )
+    ik_handle = cmds.rename(ik_handle,f"{prefix}_IKHandle_001")
+
+    # =========================
+    # IK CONTROL
+    # =========================
+
+    ik_ctrl, ik_offset = crear_control(
+        f"{prefix}_IK_CTRL_001",
+        ik_chain[-1],
+        size=tamano * 1.3,
+        color=13
+    )
+
+    cmds.parent(ik_handle, ik_ctrl)
+
+    # =========================
+    # POLE VECTOR
+    # =========================
+
+    pv_ctrl, pv_offset_grp = crear_control(
+        f"{prefix}_PV_CTRL_001",
+        ik_chain[1],
+        size=tamano * 0.7,
+        color=6
+    )
+
+    cmds.move(
+        0,
+        0,
+        pv_offset,
+        pv_offset_grp,
+        relative=True
+    )
+
+    cmds.poleVectorConstraint(
+        pv_ctrl,
+        ik_handle
+    )
+
+    # =========================
+    # CONSTRAINTS FK IK -> MAIN
+    # =========================
+    constraints = []
+
+    for fk, ik, main in zip(fk_chain[:-1],ik_chain[:-1],main_chain[:-1]):
+
+        c = cmds.orientConstraint(fk,ik,main,mo=False)[0]
+        constraints.append(c)
+
+    # =========================
+    # FKIK ATTRIBUTE
+    # =========================
+    locator = cmds.spaceLocator()[0]
+
+    shape = cmds.listRelatives(locator,shapes=True)[0]
+    shape = cmds.rename(shape,f"{prefix}_FKIK_attributes")
+
+    cmds.addAttr(shape,longName="FKIK",attributeType='double', min=0,max=1,
+        keyable=True)
+    cmds.setAttr(f"{shape}.visibility",0)
+    cmds.parent(shape,joint_attr,r=True,s=True)
+    cmds.delete(locator)
+
+    # =========================
+    # REVERSE NODE
+    # =========================
+    reverse = cmds.shadingNode('reverse', asUtility=True,n=f"{prefix}_FKIK_reverse")
+    cmds.connectAttr(f"{shape}.FKIK",f"{reverse}.inputX")
+
+    # =========================
+    # CONEXIONES
+    # =========================
+    for c in constraints:
+
+        weights = cmds.orientConstraint(c,q=True,weightAliasList=True)
+        # FK
+        cmds.connectAttr(f"{reverse}.outputX",f"{c}.{weights[0]}",force=True)
+        # IK
+        cmds.connectAttr(f"{shape}.FKIK",f"{c}.{weights[1]}",force=True)
+
+    print(f"✅ Sistema IKFK creado -> {prefix}")
+
+    return {
+        "ikHandle": ik_handle,
+        "ikControl": ik_ctrl,
+        "poleVector": pv_ctrl,
+        "attrShape": shape,
+        
+        "constraints": constraints
+    }
+
+
+
+
+def distancia_entre(a, b):
+
+    p1 = cmds.xform(a, q=True, ws=True, t=True)
+    p2 = cmds.xform(b, q=True, ws=True, t=True)
+
+    return math.sqrt(
+        (p2[0]-p1[0])**2 +
+        (p2[1]-p1[1])**2 +
+        (p2[2]-p1[2])**2
+    )
+
+
+def crear_control(nombre, target, size=1, color=17):
+
+    # círculo controlador
+    ctrl = cmds.circle(
+        n=nombre,
+        normal=[1,0,0],
+        radius=size
+    )[0]
+
+    # grupo offset
+    offset = cmds.group(ctrl, n=f"{nombre}_OFFSET")
+
+    # mover al joint
+    cmds.delete(cmds.parentConstraint(target, offset))
+
+    # color
+    shapes = cmds.listRelatives(ctrl, s=True)
+
+    for s in shapes:
+        cmds.setAttr(f"{s}.overrideEnabled", 1)
+        cmds.setAttr(f"{s}.overrideColor", color)
+
+    return ctrl, offset
